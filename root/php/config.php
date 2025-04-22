9fd5547f79f4059b53b3a82eb7ae5e24077d4bd1
muniir114 <36700187+muniir114@users.noreply.github.com>
2025-04-22 09:35:20 +0300

    add new files

--- root/php/config.php
+++ root/php/config.php
@@ -1,9 +1,13 @@
 <?php
+// Load .env variables into $_ENV
+foreach (parse_ini_file('.env') as $key => $value) {
+    $_ENV[$key] = $value;
+}
 // Database configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'cms');
+define('DB_USER', $_ENV["DB_USERNAME"]);
+define('DB_PASS', $_ENV["DB_PASSWORD"]);
+define('DB_NAME', $_ENV["DB_NAME"]);
 
 // Create connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -13,3 +17,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
